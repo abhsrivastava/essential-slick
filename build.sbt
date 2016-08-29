adc1834b6fb3f07d73671632e9bfe7613f007ffb
Abhishek Srivastava <abhsrivastava@researchnow.com>
2016-08-28 22:08:47 -0500

    added more queries

--- build.sbt
+++ build.sbt
@@ -7,7 +7,6 @@ scalaVersion := "2.11.8"
 libraryDependencies ++= Seq(
    "com.typesafe.slick" %% "slick" % "3.1.0",
    "com.typesafe.slick" %% "slick-codegen" % "3.1.0",
-   "ch.qos.logback" % "logback-classic" % "1.1.2",
    "mysql" % "mysql-connector-java" % "5.1.38"
 )
     
--- src/main/scala-2.11/com/abhi/Message.scala
+++ src/main/scala-2.11/com/abhi/Message.scala
@@ -13,29 +13,53 @@ import scala.util.{Success, Failure}
 object MySlickApp extends App {
    val db = Database.forConfig("essential-slick ")
    val messages = TableQuery[MessageTable]
+   query1
+   query2
 
-   val tableExists = MTable.getTables map { tables =>
-      tables.exists(_.name.name == messages.baseTableRow.tableName)
+   def query2: Unit = {
+      val action = messages.filter(_.sender =!= "DAVE").map(t => (t.id, t.content) <> (Foo.tupled, Foo.unapply)).result
+      val f1 = db.run(action)
+      for {
+         records <- f1
+         record <- records
+      } println(s"${record.id} ${record.content}")
    }
 
-   val dropAction = messages.schema.drop
-   val createAction = messages.schema.create
 
-   val freshMessages = Seq(
-      Message("Dave", "Hello HAL. Do you read me? HAL?"),
-      Message("HAL", "Affirmtive, Dave. I read you."),
-      Message("Dave", "Open the pod doors, HAL."),
-      Message("HAL", "I'm Sorry. Dave. I'm afraid, I cannot do that.")
-   )
-
-   val insertAction = messages ++= freshMessages
-   val finalAction = tableExists >> dropAction >> createAction >> insertAction
-   DBIO.
-   val finalFuture = db.run(finalAction)
-   finalFuture.onComplete{
-      case Success(s) => println("table initialized successfully "); db.close
-      case Failure(f) => println(f.getMessage); db.close
+   def query1: Unit = {
+      val action = messages.filter(_.sender === "HAL").map(t => t.content).result
+      val f1 = db.run(action)
+      for {
+         v <- f1
+         c <- v
+      } println(c)
    }
+
+
+   def createSchema: Unit = {
+      val tableExists = MTable.getTables map { tables =>
+         tables.exists(_.name.name == messages.baseTableRow.tableName)
+      }
+
+      val dropAction = messages.schema.drop
+      val createAction = messages.schema.create
+
+      val freshMessages = Seq(
+         Message("Dave", "Hello HAL. Do you read me? HAL?"),
+         Message("HAL", "Affirmtive, Dave. I read you."),
+         Message("Dave", "Open the pod doors, HAL."),
+         Message("HAL", "I'm Sorry. Dave. I'm afraid, I cannot do that.")
+      )
+
+      val insertAction = messages ++= freshMessages
+      val finalAction = tableExists >> dropAction >> createAction >> insertAction
+      val finalFuture = db.run(finalAction)
+      finalFuture.onComplete {
+         case Success(s) => println("table initialized successfully "); db.close
+         case Failure(f) => println(f.getMessage); db.close
+      }
+   }
+
 
    scala.io.StdIn.readLine()
 }
@@ -48,3 +72,4 @@ final case class MessageTable(tag: Tag) extends Table[Message](tag, "Message") {
    def content = column[String]("content")
    def * = (sender, content, id) <> (Message.tupled, Message.unapply)
 }
+case class Foo(id: Long, content: String)
